200dfc7b794b2576ba7cae2a7c3a039bc8cc43a7
giogio555 <jordyhutahaean555@gmail.com>
2025-09-16 15:36:17 +0700

    changes

--- app.go
+++ app.go
@@ -46,45 +46,22 @@ type Scoreboard struct {
 	Visible4    bool   `json:"visible4"`
 }
 
-// GetScoreboard returns the current scoreboard
 func (a *App) GetScoreboard() Scoreboard {
-	// Read from your internal state or file
-	return Scoreboard{
-		Game:        "SF",
-		Player1:     "Ryu",
-		Team1:       "Team 1",
-		Controller1: "P1",
-		Score1:      0,
-		Visible1:    true,
-		Player2:     "Ken",
-		Team2:       "Team 2",
-		Controller2: "P2",
-		Score2:      0,
-		Visible2:    true,
-		Player3:     "",
-		Team3:       "",
-		Controller3: "",
-		Visible3:    false,
-		Player4:     "",
-		Team4:       "",
-		Controller4: "",
-		Visible4:    false,
-	}
+	return a.scoreboard
 }
 
-// UpdateScoreboard updates the scoreboard
 func (a *App) UpdateScoreboard(data Scoreboard) {
 	a.scoreboard = data
 }
 
 func (a *App) SaveScoreboardJSON(data Scoreboard) error {
-	file, err := os.Create("scoreboard.json") // save in the app directory
+	file, err := os.Create("scoreboard.json")
 	if err != nil {
 		return err
 	}
 	defer file.Close()
 
 	encoder := json.NewEncoder(file)
-	encoder.SetIndent("", "  ") // pretty print
+	encoder.SetIndent("", "  ")
 	return encoder.Encode(data)
 }
